c1e584ba56ec4425d151bfc8399ee318398a37d5
bence <benceluzsinszky@gmail.com>
2024-10-24 14:43:57 +0200

    update path (use file instead of df later)

--- src/FileIO.py
+++ src/FileIO.py
@@ -1,11 +1,18 @@
 import pandas as pd
 from datetime import datetime
 import json
+import os
 
 
-def write_to_csv(data: pd.DataFrame):
-    file_name = datetime.now().strftime("%Y%m%d_%H%M%S.csv")
-    data.to_csv(f"../results/{file_name}", index=False)
+def write_to_csv(data: pd.DataFrame, path: str | None = None) -> None:
+    if path is None:
+        file_name = datetime.now().strftime("%Y%m%d_%H%M%S.csv")
+
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        results_dir = os.path.join(base_dir, "..", "results")
+        path = os.path.join(results_dir, file_name)
+
+    data.to_csv(path, index=False)
 
 
 def load_config_file(path: str) -> dict:
